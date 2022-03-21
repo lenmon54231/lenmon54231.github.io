7a0e130410b5b441d49b9a09507ecae7174590ee
limeng <limeng54231@163.com>
2022-03-21 16:46:47 +0800

    'autoCommit'

--- hexoCI.js
+++ hexoCI.js
@@ -2,12 +2,16 @@
 const shell = require("shelljs");
 
 const runHexoCI = async function () {
-  await shell.exec("git pull");
-  await shell.exec("git add .");
-  await shell.exec("git commit -m 'autoCommit'");
-  await shell.exec("git push");
-  await shell.exec("hexo clean");
-  await shell.exec("hexo g");
-  await shell.exec("hexo d");
+  try {
+    await shell.exec("git pull");
+    await shell.exec("git add .");
+    await shell.exec("git commit -m 'autoCommit'");
+    await shell.exec("git push");
+    await shell.exec("hexo clean");
+    await shell.exec("hexo g");
+    await shell.exec("hexo d");
+  } catch (error) {
+    console.log("CI流程报错!!!!!", error);
+  }
 };
 runHexoCI();
